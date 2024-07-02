c44a7ea0947f51ad305e1381945dcfaee162d82b
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-07-02 03:03:36 +0300

    init: create new repo

---
+++ server/seeds/data/admins/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable max-len */
+const md5 = require('md5');
+
+module.exports = {
+    login: 'admin',
+    password: md5('Aa123456'),
+    email: 'admin@pro100-capital.ru',
+    id: 'admin_id'
+};
---
+++ src/apps/client/actions/setTransactionPopup.js
@@ -0,0 +1,8 @@
+import { SET_TRANSACTION_POPUP } from '../types/types';
+
+const setTransactionsPopup = payload => ({
+    type: SET_TRANSACTION_POPUP,
+    payload
+});
+
+export default setTransactionsPopup;
